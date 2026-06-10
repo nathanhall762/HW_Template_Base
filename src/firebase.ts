cf4a525c61195c948dc14934a8023fdb7ec0ae62
Nathan Hall <nathanhall762@gmail.com>
2026-06-10 12:18:25 -0500

    cms is now selfhosted. firebase config from single source object

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
 import { getAnalytics } from 'firebase/analytics';
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import type { DocumentData } from 'firebase/firestore';
 import {
   getFirestore,
@@ -9,17 +9,26 @@ import {
   where,
 } from 'firebase/firestore/lite';
 
-const firebaseConfig = {
-  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
-  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
-  measurementId: import.meta.env.PUBLIC_FIREBASE_MEASUREMENT_ID,
+// export const firebaseConfig = {
+//   apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
+//   measurementId: import.meta.env.PUBLIC_FIREBASE_MEASUREMENT_ID,
+// };
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCtRObgRwLfVC2h1XigbP7v7TZ-pB2I4yQ',
+  authDomain: 'fart-stack-test.firebaseapp.com',
+  projectId: 'fart-stack-test',
+  storageBucket: 'fart-stack-test.firebasestorage.app',
+  messagingSenderId: '387040268558',
+  appId: '1:387040268558:web:dea070943fbfd942c8875f',
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export function initAnalytics() {
   if (window !== undefined) {
